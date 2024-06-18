790af43ce7baf42db71a4c5120d6a179f2c0edf5
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-06-18 22:50:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const Btn = document.getElementById("btn");
+const ullist = document.querySelector(".ulls");
+
+const STORE = [];
+ullist.addEventListener("click", function (e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("thru");
+  }
+});
+
+Btn.addEventListener("click", () => {
+  const inpuT = document.querySelector(".inputtext").value;
+  const li = document.createElement("li");
+  li.classList.add("listt");
+  const button = document.createElement("button");
+  button.classList.add("btnclass");
+  button.textContent = "X";
+  li.innerHTML = `<span>${inpuT}</span>`;
+  li.appendChild(button);
+
+  STORE.push(li);
+
+  button.addEventListener("click", () => {
+    const index = STORE.indexOf(li);
+    if (index > -1) {
+      STORE.splice(index, 1);
+      ullist.removeChild(li);
+    }
+  });
+
+  console.log(STORE);
+  ullist.appendChild(li);
+});
+
+// ullist.addEventListener("click", function (e) {
+//   if (e.target.tagName === "li") {
+//     e.target.classList.toggle("thru");
+//   }
+// });
