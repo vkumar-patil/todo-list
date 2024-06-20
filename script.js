c084a2cea9755b6553bb38069885da856b360ff7
Vijaykumar Patil <vijaykumarpatil960@gmail.com>
2024-06-20 19:54:12 +0530

    addlocal storeg input value

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const Btn = document.getElementById("btn");
 const ullist = document.querySelector(".ulls");
 
-const STORE = [];
+const STORE = JSON.parse(localStorage.getItem("ListItem")) || [];
+STORE.forEach((licontent) => {
+  const li = creatListItem(licontent);
+  ullist.appendChild(li);
+});
 ullist.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("thru");
@@ -10,15 +14,18 @@ ullist.addEventListener("click", function (e) {
 
 Btn.addEventListener("click", () => {
   const inpuT = document.querySelector(".inputtext").value;
+
   const li = document.createElement("li");
   li.classList.add("listt");
   const button = document.createElement("button");
   button.classList.add("btnclass");
   button.textContent = "X";
   li.innerHTML = `<span>${inpuT}</span>`;
+  localStorage.setItem("NAME", JSON.stringify(li.innerHTML));
   li.appendChild(button);
 
   STORE.push(li);
+  localStorage.setItem("listitems", JSON.stringify(STORE));
 
   button.addEventListener("click", () => {
     const index = STORE.indexOf(li);
@@ -27,7 +34,9 @@ Btn.addEventListener("click", () => {
       ullist.removeChild(li);
     }
   });
+  // localStorage.setItem("listitems", JSON.stringify(STORE));
 
   console.log(STORE);
+
   ullist.appendChild(li);
 });
